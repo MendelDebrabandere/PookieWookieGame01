b96f4b316e3223e9f2200c387f3a4e026967e38d
Mendel Debrabandere <mendel.debrabandere@hotmail.com>
2024-05-25 02:20:19 +0200

    kitty animations

--- Game/Scripts/BaseEnemy.gd
+++ Game/Scripts/BaseEnemy.gd
@@ -1,14 +1,14 @@
 extends PathFollow2D
 
 @onready var parent = get_parent() as Path2D
-
+@onready var anim = get_node("CharacterBody2D/AnimatedSprite2D")
 
 const MOVE_SPEED = 100
 
 
 func _ready():
 	set_progress(0)
-
+	anim.play("default")
 
 func _process(delta):
 	if get_progress_ratio() > 0.95:
