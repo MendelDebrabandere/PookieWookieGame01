79504d453d9e85ebfa5da33ef76673ea7c6c1baf
Mendel Debrabandere <mendel.debrabandere@hotmail.com>
2024-06-07 11:50:35 +0200

    projectile on hit effect colors

--- Game/Scripts/BaseProjectile.gd
+++ Game/Scripts/BaseProjectile.gd
@@ -17,41 +17,44 @@ func _ready():
 func _process(delta):
 	if kill_timer:
 		return
-	elif target and finished_charging:
+	else:
 		if is_instance_valid(target):
 			last_enemy_pos = target.get_global_position()
-		
-		var direction : Vector2 = (last_enemy_pos - get_global_position())
-		if direction.length() <= 1:
-			queue_free()
-		direction = direction.normalized()
-		set_global_position(get_global_position() + direction * fly_speed * delta)
+		if target and finished_charging:
+			var direction : Vector2 = (last_enemy_pos - get_global_position())
+			if direction.length() <= 1:
+				Explode()
+			direction = direction.normalized()
+			set_global_position(get_global_position() + direction * fly_speed * delta)
 
 func _on_body_entered(body):
 	if body is Enemy:
 		var health: Health = body.get_node("Health")
 		health.take_damage(5)
-		#set kill timer, using timer to allow particle effect to play first
-		kill_timer = Timer.new()
-		kill_timer.one_shot = true
-		add_child(kill_timer)
-		kill_timer.set_wait_time(1)
-		kill_timer.timeout.connect(queue_free)
-		kill_timer.start()
-		#spawn FX
-		var fx = hitFX_scene.instantiate() as CPUParticles2D
-		fx.emitting = true
-		add_child(fx)
-		#setting FX gradient
-		var gradient = load("res://Resources/Gradients/FireHitParticleGradient.tres") as Gradient
-		fx.color_ramp = gradient
-		#hide projectile
-		anim.visible = false
+		Explode()
+
 
 func SetTargetAndType(in_enemy: Enemy, in_type :Definitions.TowerType):
 	target = in_enemy
 	type = in_type
 
-
 func _on_animated_sprite_2d_animation_finished():
 	finished_charging = true
+	
+func Explode():
+	#set kill timer, using timer to allow particle effect to play first
+	kill_timer = Timer.new()
+	kill_timer.one_shot = true
+	add_child(kill_timer)
+	kill_timer.set_wait_time(1)
+	kill_timer.timeout.connect(queue_free)
+	kill_timer.start()
+	#spawn FX
+	var fx = hitFX_scene.instantiate() as CPUParticles2D
+	fx.emitting = true
+	add_child(fx)
+	#setting FX gradient
+	var gradient = load(Definitions.tower_projectile_hit_fx[type]) as Gradient
+	fx.color_ramp = gradient
+	#hide projectile
+	anim.visible = false
--- Game/Scripts/definitions.gd
+++ Game/Scripts/definitions.gd
@@ -7,5 +7,6 @@ const tower_names: Array[String] = ["Fire", "Ice", "Poison"]
 const tower_costs: Array[int] = [10, 20, 30]
 const tower_textures: Array[String] = ["res://Resources/Assets/mushroomF.png", "res://Resources/Assets/mushroomI.png", "res://Resources/Assets/mushroomP.png"]
 const tower_font_size: Array[int] = [14,14,11]
+const tower_projectile_hit_fx: Array[String] = ["res://Resources/Gradients/FireHitParticleGradient.tres", "res://Resources/Gradients/IceHitParticleGradient.tres", "res://Resources/Gradients/PoisonHitParticleGradient.tres"]
 
 const tower_fire_rate: Array[float] = [0.2,0.7,1]
